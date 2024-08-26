9219828776cee22da0085da7096518b6d28e9820
GrabowskaJoanna <joanna.trojanek18@gmail.com>
2024-08-26 13:36:43 +0200

    added pages and react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import {Route, Router, Routes, HashRouter} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <HashRouter>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path="/products" element={<Products/>}/>
+                <Route path="/products:id" element={<ProductDetails/>}/>
+            </Routes>
+        </HashRouter>
+    );
 }
 
 export default App;
